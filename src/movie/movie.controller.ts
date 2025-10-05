844827480708c627852d07133ac5496e343e3ba9
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2025-10-05 13:21:53 +0300

    add dynamic config module

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { MovieService } from './movie.service';
 
-@Controller('movie')
+@Controller('movies')
 export class MovieController {
-  constructor(private readonly movieService: MovieService) {}
-
-  @Get('test')
-  async test() {
-    return this.movieService.test();
+  @Get()
+  findAll() {
+    return [
+      { id: 1, title: 'Inception' },
+      { id: 2, title: 'The Dark Knight' },
+      { id: 3, title: 'Interstellar' },
+    ];
   }
 }
